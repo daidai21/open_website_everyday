3ab35272b47c886d0129de8183058f10b00ebd33
hu.fwh <hu.fwh@alibaba-inc.com>
2021-02-06 22:02:48 +0800

    .

--- open_website_everyday.sh
+++ open_website_everyday.sh
@@ -9,72 +9,125 @@
 # #############################################################################
 
 
-echo "第一个参数为：$1";
+# TODO: only support MacOS
+
+
+echo "Selected item ($1)";
 
 if [ -z "$1" ]
 then
     echo "Err: Need param"
+    echo "===================="
+    echo "Select Item:"
+    echo "* play"
+    echo "* job"
+    echo "* open_src"
+    echo "* buy"
+    echo "* infor"
+    echo "* discuss"
+    echo "* blog_self"
+    echo "* mail"
+    echo "===================="
     exit 1
 fi
 
-if [ "$1" = "self" ]
+if [ "$1" = "play" ]
 then
-    # self
-    open https://www.zhihu.com/
+    # video or living
+    open https://www.youtube.com/
+    # open https://www.twitch.tv/
+    open https://www.bilibili.com/
+    open https://www.acfun.cn/
 
+elif [ "$1" = "job" ]
+then
+    # v2ex
+    open https://v2ex.com/?tab=jobs
+    open https://v2ex.com/go/jobs
+
+    # 牛客
+    open https://www.nowcoder.com/intern/center
+    open https://www.nowcoder.com/fulltime/center
+
+    # boss
+    open https://www.zhipin.com/
+
+    # TODO: maimai
+
+    # 拉钩
+    open https://www.lagou.com/
+
+    # 领英
+    open https://www.linkedin.com/
+
+elif [ "$1" = "open_src" ]
+then
+    # 找潜力项目
+    open https://github.com/trending/java?since=monthly
+    open https://github.com/trending/python?since=monthly
+    open https://github.com/trending/c++?since=monthly
+
+    # 找大佬
+    open https://github.com/trending/developers/c++?since=monthly
+    open https://github.com/trending/developers/python?since=monthly
+    open https://github.com/trending/developers/java?since=monthly
+
+    # github
+    open https://github.com
     open https://github.com/trending
-    # open https://github.com/trending?spoken_language_code=zh
-    # open https://github.com/trending/java?since=daily&spoken_language_code=zh
-    # open https://github.com/trending/c?since=daily
-    # open https://github.com/trending/c++?since=daily
-    # open https://github.com/trending/java?since=daily
-    # open https://github.com/trending/go?since=daily
-    # open https://github.com/trending/python?since=daily
-
-    open https://ourcoders.com/
-
-    open https://github.com/
+    open https://github.com/collections
     open https://github.com/explore
 
-    open http://mail.aliyun.com/
+elif [ "$1" = "buy" ]
+then
+    # 什么值得买
+    open https://www.smzdm.com/
 
-    open https://v2ex.com/
-    # open https://v2ex.com/?tab=tech
-    # open https://v2ex.com/?tab=jobs
-    # open https://v2ex.com/go/jobs
 
-    # open https://weibo.com/
+elif [ "$1" = "infor" ]
+then
 
-    # open https://www.nowcoder.com/
-    # open https://www.nowcoder.com/discuss?type=3
+    open https://ohmyrss.com/#
 
-    # open https://medium.com/
-
-    # pd 论坛
-    open https://www.pmcaff.com/
-    # open https://mail.google.com/
-
-    # open https://www.douban.com/
-    # open https://www.douban.com/group/
-    # open https://www.douban.com/gallery/
-
-    # blog
-    # open https://netflixtechblog.com/
-
-    # 产品经理、运营 论坛
-    # open http://www.woshipm.com/
+    # 知乎热榜
+    open https://www.zhihu.com/hot
 
     # 各个平台的热榜
     open https://tophub.today/
 
+
+
+elif [ "$1" = "discuss" ]
+then
+    # 产品经理、运营 论坛
+    open http://www.woshipm.com/
+
+    open https://medium.com/
+
     open https://www.infoq.cn/
+
+    open https://www.douban.com/
+
+    open https://www.zhihu.com/
+
+    open https://ourcoders.com/
+
+    open https://v2ex.com/
+
+    # 牛客讨论区
+    open https://www.nowcoder.com/discuss
+    # 牛客讨论区 - 工作以后
+    open https://www.nowcoder.com/discuss?type=12&order=0
+
+    # pd 论坛
+    open https://www.pmcaff.com/
 
 
 elif [ "$1" = "blog" ]
 then
     # blog
 
-    # 技术blog
+    # 技术blog 解道
     open https://www.jdon.com/
 
 
@@ -82,42 +135,33 @@ then
     open http://ifeve.com/
 
     # java知音
-    # open https://www.javazhiyin.com/
-
+    open https://www.javazhiyin.com/
 
     # open https://pythonhunter.org/
 
     # 一个很酷的modern c++开源社区
     # open http://purecpp.org/
 
-    # 
-    open https://ohmyrss.com/#
-
     # tester discuss
     open https://testerhome.com/
 
     open https://www.infoq.com/
 
-elif [ "$1" = "blog.self" ]
+elif [ "$1" = "blog_self" ]
 then
 
     # 构建我的被动收入
-    # open https://www.bmpi.dev/
+    open https://www.bmpi.dev/
 
     # 云原生实验室
-    # https://fuckcloudnative.io/
+    https://fuckcloudnative.io/
 
-    # https://laike9m.com/blog/about/
+    https://laike9m.com/blog/about/
 
-elif [ "$1" = "play" ]
+elif [ "$1" = "mail" ]
 then
+    open http://mail.aliyun.com/
 
-    # video
-    open https://www.youtube.com/
-    # open https://www.twitch.tv/
-    open https://www.bilibili.com/
-    open https://www.acfun.cn/
+    open https://mail.google.com/
 
-    # text
-    # open https://www.xinli001.com/
 fi
